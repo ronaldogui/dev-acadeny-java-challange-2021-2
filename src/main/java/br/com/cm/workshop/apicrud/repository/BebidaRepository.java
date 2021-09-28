4c9f12ab2a77f170168ae9a8c8ce9602da0cbb9f
sidney <sidnobre.sdn@gmail.com>
2021-09-28 16:10:01 -0300

    Projeto base

--- src/main/java/br/com/cm/workshop/apicrud/repository/BebidaRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.cm.workshop.apicrud.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-import br.com.cm.workshop.apicrud.model.Bebida;
-
-@Repository
-public interface BebidaRepository extends JpaRepository<Bebida, Long> {}
