ba7f343cde24a5190bdb06a7dd9255ed4800d67d
07042613325 <ronaldogui25@gmail.com>
2021-10-01 03:07:31 -0300

    Atualizando alguns metodos na classe NotaFiscalService

--- src/main/java/br/com/cm/workshop/apicrud/service/NotaFiscalService.java
+++ src/main/java/br/com/cm/workshop/apicrud/service/NotaFiscalService.java
@@ -3,7 +3,6 @@ package br.com.cm.workshop.apicrud.service;
 import br.com.cm.workshop.apicrud.enums.Status;
 import br.com.cm.workshop.apicrud.model.NotaFiscal;
 import br.com.cm.workshop.apicrud.repository.NotaFiscalRepository;
-import org.aspectj.weaver.ast.Not;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.ReflectionUtils;
@@ -20,77 +19,103 @@ public class NotaFiscalService {
     @Autowired
     private NotaFiscalRepository notaFiscalRepository;
 
-    public NotaFiscal criaNotaFiscal(NotaFiscal notaFiscal){
+    public NotaFiscal criaNotaFiscal(NotaFiscal notaFiscal) {
         notaFiscal.setStatus(Status.PENDENTE);
         return notaFiscalRepository.saveAndFlush(notaFiscal);
     }
 
-    public List<NotaFiscal> listaTodasNotasFiscais(){
+    public List<NotaFiscal> listaTodasNotasFiscais() {
         return notaFiscalRepository.findAll();
     }
 
-    public Optional<NotaFiscal> buscaPorId(Long id){
-        if(notaFiscalRepository.existsById(id)){
+    public Optional<NotaFiscal> buscaPorId(Long id) {
+        if (notaFiscalRepository.existsById(id)) {
             return notaFiscalRepository.findById(id);
-        }else
+        } else
             throw new EntityNotFoundException("A nota fiscal com a identificação informada não existe ! ");
     }
 
-    public NotaFiscal alteraNotaFiscal(Long id,NotaFiscal notaFiscal){
-        if(notaFiscalRepository.existsById(id)){
-            if(id.equals(notaFiscal.getId()))
+    public NotaFiscal alteraNotaFiscal(Long id, NotaFiscal notaFiscal) {
+        if (notaFiscalRepository.existsById(id)) {
+            if (id.equals(notaFiscal.getId()))
                 return notaFiscalRepository.saveAndFlush(notaFiscal);
             else
                 throw new UnsupportedOperationException("Identificação informada é diferente da identificação da nota fiscal.");
-        }else
+        } else
             throw new EntityNotFoundException("Nota fiscal não encontrada.");
     }
 
-    public NotaFiscal alteraStatusDeNotaFiscal(Long id, Map<Object,Object> campos){
-        if(notaFiscalRepository.existsById(id)){
-               Optional<NotaFiscal> optional = notaFiscalRepository.findById(id);
-               if(optional.isPresent()){
-                   NotaFiscal notaFiscal = optional.get();
-                   campos.forEach((k,v)->{
-                       Field field = ReflectionUtils.findField(NotaFiscal.class,(String) k);
-                       field.setAccessible(true);
-                       ReflectionUtils.setField(field,notaFiscal,verificaStatus(v.toString()));
-                   });
-                   return notaFiscalRepository.saveAndFlush(notaFiscal);
-               }else
-                   throw new EntityNotFoundException("A nota fiscal com a identificação informada não foi encontrada ! ");
-        }else
+    public NotaFiscal alteraStatusDeNotaFiscal(Long id, Map<Object, Object> campos) {
+        if (notaFiscalRepository.existsById(id)) {
+            Optional<NotaFiscal> optional = notaFiscalRepository.findById(id);
+            if (optional.isPresent()) {
+                NotaFiscal notaFiscal = optional.get();
+                campos.forEach((k, v) -> {
+                    Field field = ReflectionUtils.findField(NotaFiscal.class, (String) k);
+                    field.setAccessible(true);
+                    if(regrasStatus(notaFiscal,v.toString()))
+                        ReflectionUtils.setField(field, notaFiscal, verificaStatus(v.toString()));
+                });
+                return notaFiscalRepository.saveAndFlush(notaFiscal);
+            } else
+                throw new EntityNotFoundException("A nota fiscal com a identificação informada não foi encontrada ! ");
+        } else
             throw new EntityNotFoundException("A nota fiscal com a identificação informada não existe ! ");
     }
 
-    public void deletaNotaFiscal(Long id){
+    public void deletaNotaFiscal(Long id) {
         Optional<NotaFiscal> notaFiscal = notaFiscalRepository.findById(id);
-        if(!notaFiscal.isPresent())
+        if (!notaFiscal.isPresent())
             throw new EntityNotFoundException("A nota fiscal com a identificação informada não existe ! ");
         notaFiscalRepository.deleteById(id);
     }
 
-    public Status verificaStatus(String status){
+    public Status verificaStatus(String status) {
         Status statusAtualizado;
-        switch (status){
+        switch (status) {
             case "PENDENTE":
                 statusAtualizado = Status.PENDENTE;
                 break;
             case "EM_PROCESSAMENTO":
-                statusAtualizado =  Status.EM_PROCESSAMENTO;
+                statusAtualizado = Status.EM_PROCESSAMENTO;
                 break;
             case "APROVADA":
-                statusAtualizado =  Status.APROVADA;
+                statusAtualizado = Status.APROVADA;
                 break;
             case "COM_ERRO":
-                statusAtualizado =  Status.COM_ERRO;
+                statusAtualizado = Status.COM_ERRO;
                 break;
             case "CANCELADA":
-                statusAtualizado =  Status.CANCELADA;
+                statusAtualizado = Status.CANCELADA;
                 break;
             default:
                 throw new UnsupportedOperationException("Esse status não existe");
         }
         return statusAtualizado;
     }
+
+    public Boolean regrasStatus(NotaFiscal notaFiscal, String statusNovo) {
+
+        Boolean retorno = false;
+
+        if (statusNovo.equals("CANCELADA") && (notaFiscal.getStatus().equals(Status.EM_PROCESSAMENTO) || notaFiscal.getStatus().equals(Status.CANCELADA)))
+            throw new UnsupportedOperationException("Você não pode alterar o status para 'CANCELADA' pois o status atual está em 'EM_PROCESSAMENTO' ou 'CANCELADA' ");
+        else if (statusNovo.equals("CANCELADA") && (notaFiscal.getStatus().equals(Status.EM_PROCESSAMENTO) || notaFiscal.getStatus().equals(Status.CANCELADA))){
+            retorno = true;
+        }else if (statusNovo.equals("EM_PROCESSAMENTO") && (notaFiscal.getStatus().equals(Status.PENDENTE) || notaFiscal.getStatus().equals(Status.COM_ERRO))) {
+            retorno = true;
+        } else if (statusNovo.equals("EM_PROCESSAMENTO") && !(notaFiscal.getStatus().equals(Status.PENDENTE) || notaFiscal.getStatus().equals(Status.COM_ERRO)))
+            throw new UnsupportedOperationException("Você não pode alterar o status para 'EM_PROCESSAMENTO' pois o status atual está em 'CANCELADA' ou 'APROVADA' ");
+        if (statusNovo.equals("APROVADA") && notaFiscal.getStatus().equals(Status.EM_PROCESSAMENTO)) {
+            retorno = true;
+        }else  if (statusNovo.equals("APROVADA") && !(notaFiscal.getStatus().equals(Status.EM_PROCESSAMENTO))){
+            throw new UnsupportedOperationException("Você não pode alterar o status para 'APROVADA' pois o status atual não está em 'EM_PROCESSAMENTO' ");
+        }else {
+            retorno = true;
+        }
+
+        return retorno;
+    }
+
+
 }
